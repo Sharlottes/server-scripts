bd8e00cfe4bc87cf277545a0e73c5ffc961437fc
sharlottes <seeseafood@naver.com>
2021-10-16 20:53:34 +0900

    update

--- backup.js
+++ backup.js
@@ -1,4 +1,5 @@
-url='https://raw.githubusercontent.com/Sharlottes/server-scripts/master/server-scripts.js'
-url='https://raw.githubusercontent.com/Sharlottes/server-scripts/master/commands.js'
-url='https://raw.githubusercontent.com/Sharlottes/server-scripts/master/fx.js'
-Core.net.httpGet(url, r => Vars.mods.getScripts().runConsole(r.getResultAsString()), err=>{})
+preurl='https://raw.githubusercontent.com/Sharlottes/server-scripts/master/';
+Core.net.httpGet(preurl+'server-scripts.js',r=>Vars.mods.getScripts().runConsole(r.getResultAsString()),err=>{})
+Core.net.httpGet(preurl+'commands.js',r=>Vars.mods.getScripts().runConsole(r.getResultAsString()),err=>{})
+Core.net.httpGet(preurl+'fx.js',r=>Vars.mods.getScripts().runConsole(r.getResultAsString()),err=>{})
+initcmds();
