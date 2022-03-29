c1ed4242443caa315a508ec594f4cb2d9e94a61c
sharlottes <seeseafood@naver.com>
2022-03-29 21:00:52 +0900

    update

--- ai.js
+++ ai.js
@@ -13,26 +13,26 @@ var initformat=(i, amount, type, player, speed, spacing, commandAll)=>{
 
   player.unit().command(new Formation(new Vec3(player.x, player.y, 0), new JavaAdapter(FormationPattern, {
     calculateSlotLocation(out, slot){
-      angle+=speed||0.3;
+      angle+=speed;
       var radian = angle / 360 * slot/this.slots * Mathf.degRad;
       equs[i](out, radian);
       return out;
     }
   })), units);
 
-  player.unit().formation.pattern.spacing = spacing||80;
-}
+  player.unit().formation.pattern.spacing = spacing;
+};
 
-//나비 방정식
+/*나비 방정식*/
 var nabi=(out, radian)=>{
   const sizeScaling = 1;
   const rotateSpeed = 0.005;
   const equ = sizeScaling * (Mathf.pow(Mathf.E, Mathf.sin(radian)) - 2 * Mathf.cos(4 * radian) + Mathf.pow(Mathf.sin((2 * radian - Mathf.PI) / 24), 5));
 
   out.set(Tmp.v1.set(this.spacing * equ * Mathf.cos(radian), this.spacing * equ * Mathf.sin(radian)).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//별의 방정식 베타(비교차)
+/*별의 방정식 베타(비교차)*/
 var starBeta=(out, radian)=>{
   const sizeScaling = 0.5;
   const rotateSpeed = 0.005;
@@ -40,17 +40,17 @@ var starBeta=(out, radian)=>{
   const equ = sizeScaling * (Mathf.sin(5 * radian) + a);
 
   out.set(Tmp.v1.set(this.spacing * equ * Mathf.cos(radian), this.spacing * equ * Mathf.sin(radian)).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//하트 방정식
+/*하트 방정식*/
 var heart=(out, radian)=>{
   const sizeScaling = 0.5;
   const rotateSpeed = 0.001;
 
   out.set(Tmp.v1.set(this.spacing * sizeScaling * 16 * Mathf.pow(Mathf.sin(radian), 3), this.spacing * (sizeScaling * 13 * Mathf.cos(radian) - sizeScaling * 5 * Mathf.cos(2 * radian) - sizeScaling * 2 * Mathf.cos(3 * radian) - sizeScaling * Mathf.cos(4 * radian))).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//연꽃 방정식
+/*연꽃 방정식*/
 var flower=(out, radian)=>{
   const sizeScaling = 0.25;
   const rotateSpeed = 0.005;
@@ -59,17 +59,17 @@ var flower=(out, radian)=>{
   const value = value1/value2;
 
   out.set(Tmp.v1.set(this.spacing * (value1 * Mathf.cos(radian) - value2 * Mathf.cos(value * radian)), this.spacing * (value1 * Mathf.sin(radian) - value2 * Mathf.sin(value * radian))).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//곡선별 방정식(교차)
+/*곡선별 방정식(교차)*/
 var curvStar=(out, radian)=>{
   const sizeScaling = 0.25;
   const rotateSpeed = 0.01;
 
   out.set(Tmp.v1.set(this.spacing * (sizeScaling * 5 * Mathf.cos(2 * radian) + sizeScaling * 2 * Mathf.cos(3 * radian)), this.spacing * (sizeScaling * 2 * Mathf.sin(3 * radian) - sizeScaling * 5 * Mathf.sin(2 * radian))).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//사인도배 별의 방정식
+/*사인도배 별의 방정식*/
 var sinStar=(out, radian)=>{
   const sizeScaling = 1;
   const rotateSpeed = 0.005; /*max*/
@@ -80,18 +80,18 @@ var sinStar=(out, radian)=>{
   for(var i = 0; i < 2; i++) vec.rotateRad(radian);
 
   out.set(vec, 0);
-}
+};
 
-//다중 별의 방정식
+/*다중 별의 방정식*/
 var multiStar=(out, radian)=>{
   const sizeScaling = 1;
   const rotateSpeed = 0.005; /*max*/
   const equ = sizeScaling * (slot % 10) * Mathf.cos(Mathf.sin(2.5 * (radian - 0.5 * Mathf.PI)));
 
   out.set(Tmp.v1.set(this.spacing * equ * Mathf.cos(radian), this.spacing * equ * Mathf.sin(radian)).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//다엽장미곡선 방정식
+/*다엽장미곡선 방정식*/
 var flowmulti=(out, radian)=>{
   const sizeScaling = 1;
   const rotateSpeed = 0.005; /*max*/
@@ -101,9 +101,9 @@ var flowmulti=(out, radian)=>{
   const equ = sizeScaling * size * Mathf.cos(k * radian);
 
   out.set(Tmp.v1.set(this.spacing * equ * Mathf.cos(radian), this.spacing * equ * Mathf.sin(radian)).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
-//2파이 네잎클로버 방정식
+/*2파이 네잎클로버 방정식*/
 var twopi=(out, radian)=>{
   const sizeScaling = 1;
   const rotateSpeed = 0.005; /*max*/
@@ -111,9 +111,9 @@ var twopi=(out, radian)=>{
   const equ = sizeScaling * (radian + 1000 % (Mathf.PI * 2));
 
   out.set(Tmp.v1.set(this.spacing * size * Mathf.cos(2 * equ) * Mathf.cos(equ), this.spacing * size * Mathf.cos(2 * equ) * Mathf.sin(equ)).rotateRad(Time.time * rotateSpeed), 0);
-}
+};
 
 var equs=[nabi, starBeta, heart, flower, curvStar, sinStar, multiStar, flowmulti, twopi];
 
 
-initformat(3, 500, UnitTypes.flare, sharp);
+initformat(3, 500, UnitTypes.flare, sharp,0.3,80,false);
